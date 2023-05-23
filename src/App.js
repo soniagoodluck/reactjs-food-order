d0755c00ac8f2a1e607000813c23b7504d04a9bd
Sonia Goodluck <soniagoodluck@yahoo.co.uk>
2023-05-23 21:41:34 +0100

    added cart visibility stste

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useState } from 'react'; 
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  function showCartHandle(){
+    setCartIsShown(true);
+  }
+
+function hideCartHandle(){
+  setCartIsShown(false);
+
+}
+
   return (
     <CartProvider>
-      <Header />
+      {cartIsShown && <div>Cart...</div>}
+      <Header onShowCart={showCartHandle} />
       <main>
         <Meals />
       </main>
